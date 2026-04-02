770dbd83c693609c351d2747cb851ad16dfbf2be
Chris Holden <ceholden@gmail.com>
2026-04-02 16:04:16 -0400

    fix: organize slurm script in scripts/

--- src/fmask/cli/fmask_batch.py
+++ src/fmask/cli/fmask_batch.py
@@ -39,7 +39,7 @@ import os
 from pathlib import Path
 import click
 import glob
-from .fmask import fmask_physical, fmask_lightgbm, fmask_unet, fmask_lpl,  fmask_lpu, fmask_upl, fmask_upu
+from fmask.cli.fmask import fmask_physical, fmask_lightgbm, fmask_unet, fmask_lpl,  fmask_lpu, fmask_upl, fmask_upu
 
 @click.command()
 @click.option("--model", "-m", type=str, help="Cloud detection model to use.", default="UPL")
